164790bee85345915ea708f9671e16e513cb986d
otto <ottokafka@yahoo.com>
2026-07-20 03:00:23 +0800

    Add macOS AppDelegate to manage application lifecycle events

--- App/App.swift
+++ App/App.swift
@@ -3,8 +3,20 @@ import SwiftUI
 import DeveloperChatbotCore
 #endif
 
+#if os(macOS)
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
+    }
+}
+#endif
+
 @main
 struct DeveloperChatbotApp: App {
+    #if os(macOS)
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    #endif
+
     var body: some Scene {
         WindowGroup {
             ContentView()
